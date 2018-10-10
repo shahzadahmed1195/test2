5d9c5ecb719733442d31b2d72036d7dd31e59094
s git config --global user.name shahzadahmed1195 <Shahzadahmed1195>
2018-10-10 16:22:15 -0400

    first commit

---
+++ src/main/java/CucumberTesty/ShahzadAhmedCucumber/App.java
@@ -0,0 +1,13 @@
+package CucumberTesty.ShahzadAhmedCucumber;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ target/test-classes/test.Feature.feature
@@ -0,0 +1,5 @@
+Feature: Simple feature
+
+Scenario: Go to QAShahin Youtube Page
+Given I go to "QASHAHIN"
+Then I should be on "QAShahin" youtube page 
